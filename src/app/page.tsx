b65ab154252734638771f9270f80415a959f4c9e
Roy Michael <roym@totocard.com>
2025-04-01 14:18:11 +0300

    ⚙️ Setup: Tailwind, Next.js, Docker + Initial UI test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {/* כותרת לבדיקה אם Tailwind עובד */}
+        <h1 className="text-4xl text-blue-600 font-bold">Tailwind is working! 💙</h1>
+
         <Image
           className="dark:invert"
           src="/next.svg"
